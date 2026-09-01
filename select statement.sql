0c3c8a8d6cafdeb4eb9334ba41935433c5ec1ff3
ebenezerkojo-aggrey <ebenezerkojoaggrey.123@gmail.com>
2026-09-01 02:42:14 +0200

    Add files via upload

---
+++ select statement.sql
@@ -0,0 +1,32 @@
+select *
+from employee_demographics;
+
+select *
+from parks_and_recreation.employee_salaryPRIMARYcountrylanguage;
+
+
+select 
+first_name,
+last_name,
+birth_date,
+age,
+(age + 10)*10
+from parks_and_recreation.employee_demographics;
+#pemdas
+# distinct. it selects only the unique values within a column.
+
+select distinct gender
+from employee_demographics;
+
+
+
+
+
+
+
+
+
+
+
+
+
